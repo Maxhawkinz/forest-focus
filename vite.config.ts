c2d3d5757cf328ceead5c877c0fd02e853747b23
Ajay <ajaythakare500@gmail.com>
2025-08-19 23:21:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/forest-focus/',   // 👈 repo name for GitHub Pages
+  base: '/forest-focus/',   // 👈 repo name
 })
